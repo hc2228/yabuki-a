cf4d9db43b8facbd08eff6b4ba17ee0153926827
1942024 <s1942024GC@s.chibakoudai.jp>
2021-06-18 17:28:34 +0900

    sql

--- mydb.sql
+++ mydb.sql
@@ -1,103 +1,71 @@
--- MariaDB dump 10.19  Distrib 10.4.18-MariaDB, for Win64 (AMD64)
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: mydb
--- ------------------------------------------------------
--- Server version	10.4.18-MariaDB
+-- ホスト: 127.0.0.1
+-- 生成日時: 2021-06-18 10:27:24
+-- サーバのバージョン： 10.4.18-MariaDB
+-- PHP のバージョン: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `mytable`
+-- データベース: `mydb`
 --
 
-DROP TABLE IF EXISTS `mytable`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `mytable` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `foo` varchar(100) DEFAULT NULL,
-  `bar` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `mytable`
+-- テーブルの構造 `testtable`
 --
 
-LOCK TABLES `mytable` WRITE;
-/*!40000 ALTER TABLE `mytable` DISABLE KEYS */;
-INSERT INTO `mytable` VALUES (2,'い',-200),(3,'う',300),(5,'い',-200),(6,'う',300);
-/*!40000 ALTER TABLE `mytable` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `table1`
---
-
-DROP TABLE IF EXISTS `table1`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `table1` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `varcharA` varchar(40) NOT NULL,
+CREATE TABLE `testtable` (
+  `id` int(11) NOT NULL,
+  `varcharA` varchar(10) NOT NULL,
   `intA` int(11) NOT NULL,
   `intB` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `intC` int(11) NOT NULL,
+  `intD` int(11) NOT NULL,
+  `intE` int(11) NOT NULL,
+  `intF` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `table1`
+-- テーブルのデータのダンプ `testtable`
 --
 
-LOCK TABLES `table1` WRITE;
-/*!40000 ALTER TABLE `table1` DISABLE KEYS */;
-INSERT INTO `table1` VALUES (1,'A',1280,1),(2,'B',2980,0),(3,'C',198,3),(4,'D',3980,5),(5,'E',990,121),(6,'F',1500,100),(7,'G',1980,52),(8,'H',256,22),(9,'I',512,27),(10,'J',3333,4);
-/*!40000 ALTER TABLE `table1` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `testtable` (`id`, `varcharA`, `intA`, `intB`, `intC`, `intD`, `intE`, `intF`) VALUES
+(1, '山田太郎', 72250, 20000, 30000, 52250, 200000, 147750);
 
 --
--- Table structure for table `userinfo`
+-- ダンプしたテーブルのインデックス
 --
 
-DROP TABLE IF EXISTS `userinfo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `userinfo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(30) NOT NULL,
-  `passwd` varchar(60) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+--
+-- テーブルのインデックス `testtable`
+--
+ALTER TABLE `testtable`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Dumping data for table `userinfo`
+-- ダンプしたテーブルの AUTO_INCREMENT
 --
 
-LOCK TABLES `userinfo` WRITE;
-/*!40000 ALTER TABLE `userinfo` DISABLE KEYS */;
-INSERT INTO `userinfo` VALUES (1,'user1','pass1'),(2,'user2','pass2'),(3,'admin','pass3');
-/*!40000 ALTER TABLE `userinfo` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- テーブルの AUTO_INCREMENT `testtable`
+--
+ALTER TABLE `testtable`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-06-18 16:15:37
