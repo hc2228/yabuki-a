1177cb9c780252b0205e3f665e5977a40a275b08
TOMIZWA <s1942077gv@s.chibakoudai.jp>
2021-06-25 14:04:42 +0900

    a

--- htdocs/input.php
+++ htdocs/input.php
@@ -1,1 +1,17 @@
-hello!
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel='stylesheet' href='input.css' />
+    <title>データの入力</title>
+  </head>
+  <body>
+  <form method="post" action="index.html">
+
+  <?php
+
+  ?>
+
+  </form>
+  </body>
+</html>
