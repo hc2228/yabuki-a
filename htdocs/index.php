a9606256da30d3291269ea8d68de9ae262a00c95
1942024 <s1942024GC@s.chibakoudai.jp>
2021-06-30 03:49:10 +0900

    あ

--- htdocs/index.php
+++ htdocs/index.php
@@ -37,23 +37,23 @@
     <tbody>
         <tr>
             <th>給料</th>
-            <td>$z　</td>
+            <td>$z</td>
         </tr>
          <tr>
             <th>出費</th>
-            <td>$p　</td>
+            <td>$p</td>
         </tr>
          <tr>
             <th>残り予算</th>
-            <td>$s　</td>
+            <td>$s</td>
         </tr>
         <tr>
             <th>残金</th>
-            <td>$r　</td>
+            <td>$r</td>
         </tr>
         <tr>
             <th>目標まで</th>
-            <td>$w　</td>
+            <td>$w</td>
         </tr>
         <tr>
             コメントが入ります
