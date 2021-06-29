860b6df2ab2e260c2934a1d8e469cd7513798006
1942024 <s1942024GC@s.chibakoudai.jp>
2021-06-30 03:33:53 +0900

    testphp

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,6 +8,17 @@
   </head>
 
   <body>
+  <div id="header">
+		  <h1 style="text-align:center;">お財布管理ツール</h1>
+		  <h2>※千葉工業大学PM学科3年の実験サイトです</h2>
+	  </div>
+      <div id="daily">
+        
+        <?php
+            echo date('Y年m月d日');
+        ?>
+        <br>
+    </div>
       <?php
       require'db.php';
       $sql = 'SELECT*FROM testtable';
@@ -15,51 +26,44 @@
       $prepare->execute();
       $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
       
-    <div id="header">
-		  <h1 style="text-align:center;">お財布管理ツール</h1>
-		  <h2>※千葉工業大学PM学科3年の実験サイトです</h2>
-	  </div>
-    <div id="daily">
-        
-        
-            echo date('Y年m月d日');
-        
-    </div>
-        </body>
-        <br>
-  <table>
-    <tbody>
-        <tr>
-            <th>給料</th>
-            <td>テキストが</td>
-        </tr>
-         <tr>
-            <th>出費</th>
-            <td>テキストが入ります</td>
-        </tr>
-         <tr>
-            <th>残り予算</th>
-            <td>テキストが入ります</td>
+    foreach($result as $row){
+        $z =h($row['']);
+        $p = h($row['']);
+        $s = h($row['']);
+        $r = h($row['']);
+        $w = h($row['']);
+        echo 
+        "<tr>
+        <th>給料</th>
+        <td>$z</td>
         </tr>
         <tr>
-            <th>残金</th>
-            <td>テキストが入ります</td>
+        <th>出費</th>
+        <td>$p</td>
         </tr>
         <tr>
-            <th>目標まで</th>
-            <td>テキストが入ります</td>
+        <th>残り予算</th>
+        <td>$s</td>
         </tr>
         <tr>
-            <th></th>
-            <td>テキストが入ります</td>
+        <th>残金</th>
+        <td>$r</td>
         </tr>
-    </tbody>    
-</table>
-  
-</div>
+        <tr>
+        <th>残り予算</th>
+        <td>$w</td>
+        </tr>
+        <tr>
+        <th></th>
+        <td>コメントが入ります</td>
+        </tr>
+        "
+
+    }
+    ?>
 <p style="text-align: center;">
     <a href="input.php" class="btn btn-flat"><span>入力画面</span></a>
 </p>
-?>
+
 </body>
 </html>
