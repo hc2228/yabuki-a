868e9e5281d0b94941f3c9dc45a8212f52c9ba1c
shibuyafuuka <s1942054tf@s.chibakoudai.jp>
2021-07-05 09:49:45 +0900

    a

--- htdocs/show-all1.php
+++ htdocs/show-all1.php
@@ -8,7 +8,7 @@
   <body>
 <?php
 require 'db.php';                               # 接続
-$sql = 'SELECT * FROM testtable';                  # SQL文
+$sql = 'SELECT * FROM testtable';               # SQL文
 $prepare = $db->prepare($sql);                  # 準備
 $prepare->execute();                            # 実行
 $result = $prepare->fetchAll(PDO::FETCH_ASSOC); # 結果の取得
@@ -17,13 +17,13 @@ foreach ($result as $row) {
   $id       = h($row['id']);
   $userid   = h($row['userid']);
   $varcharA = h($row['varcharA']);
-  $intA     = h($row['hwork']);
-  $intB     = h($row['worktime']);
-  $intC     = h($row['target']);
-  $intD     = h($row['remonth']);
-  $intE     = h($row['inget']);
-  $intF     = h($row['fixcost']);
-  $intG     = h($row['varcost']);
+  $hwork     = h($row['hwork']);
+  $worktime     = h($row['worktime']);
+  $target     = h($row['target']);
+  $remonth     = h($row['remonth']);
+  $inget     = h($row['inget']);
+  $fixcost     = h($row['fixcost']);
+  $varcost     = h($row['varcost']);
 
 
   echo "{$id},{$userid}, {$varcharA}, {$hwor}, {$worktime}, {$target}, {$remonth}, {$inget}, {$fixcost},{$varcost}<br/>";
