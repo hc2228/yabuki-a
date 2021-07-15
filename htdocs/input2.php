4433642bdbbcf72bd119c68ca1f5f48d759df071
shibuyafuuka <s1942054tf@s.chibakoudai.jp>
2021-07-15 11:54:03 +0900

    b

---
+++ htdocs/input2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+    <link rel='stylesheet' href='home.css' />
+    <title>データの入力</title>
+  </head>
+  <body>
+  <div id="header">
+    <h1 style="text-align:center;">情報を入力してください</h1>
+    <h2>※千葉工業大学PM学科3年の実験サイトです</h2>
+   </div>
+      <?php
+            echo date('Y年m月d日');
+            $date = date("m"); //上の日付と同じ日を入れたい
+        ?>
+  <form method="post" action="index.php">
+  <table border="1" width="300" >
+    <tr>
+      <td rowspan="2">今月の給料</td>
+      <!-- セルを縦に2つ結合する -->
+      
+    <tr>
+    <th>労働時間<br>＜時＞</th>
+          <td><input name="worktime" type="text" /></td>  <!--↑のはページ別なら関係なし-->
+    </tr>
+  
+   <tr>
+   <td rowspan="2">今月の出費</td>
+  
+    <th>変動費<br>＜円＞</th>
+          <td><input name="varcost" type="text" /></td>
+</tr>
+      </table>
+
+<p style="text-align: center;">   
+    <input type="submit" class="btn btn-flat" value="送信" />
+    <input type="reset" class="btn btn-flat" value="リセット" />
+</p>
+    
+  </form>
+  </body>
+</html>
